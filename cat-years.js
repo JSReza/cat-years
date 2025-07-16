561b8d9ecc973108d44a981c14545c34352c2693
Jaheen Reza <rezajaheen@gmail.com>
2025-07-15 20:29:18 -0400

    had to fix lbs to kgs conversion

--- cat-years.js
+++ cat-years.js
@@ -1,38 +1,26 @@
-const value = {catYearInput: 0, catYearOutput: 0, lifeStage: ' '}
-const catYearsCalculator = function(value){
-    let output = 0
-    const num = parseInt(value.catYearInput)
-        if(num == 0){
-            output = 'less than 15'
-        }else if(num == 1){
-            output = 15
-        }else if(num == 2){
-            output = 24
-        }else{
-            const mult = num - 2
-            output = (mult * 4) + 24
-        }
-        value.catYearOutput = output
-    
-}
-const displayOutput = function(value){
-     document.getElementById('results').innerHTML = ''
-    if((value.catYearInput !== 0) || !(value.catYearInput) || !isNaN(value.catYearInput)){
-       const out =  document.createElement('h2')
-       out.textContent = `Your cat is ${value.catYearInput} years old in cat years, so that means that they're ${value.catYearOutput} years old in human years`
-       document.getElementById('results').appendChild(out)
-    }else{
-        const none = document.createElement('h2')
-        none.textContent = 'No values added yet'
-        document.getElementById('results').appendChild(none)
-    }
 
-}
-document.querySelector('#form').addEventListener('input', function(e){
-    value.catYearInput = e.target.value  
-    catYearsCalculator(value)
-    displayOutput(value)
+document.getElementById('form').addEventListener('submit', function(e){
+    e.preventDefault()
+    let valueEl = document.getElementsByClassName('input')
+    let resultsEl =  document.getElementById('results')
+   let value = parseInt(valueEl.value)
+    let output = catYearCalculator(value)
+   resultsEl.textContent = `Your cat is ${output} years old in human years if they are ${value} years old in cat years`
+
 })
 
-displayOutput(value)
-catYearsCalculator(value)
+function catYearCalculator(value){
+    let output = 0
+        if(value == 0){
+            output = 'less than 15'
+        }else if(value == 1){
+            output = 15
+        }else if(value == 2){
+            output = 24
+        }else{
+            let mult = num - 2
+            output = (mult * 4) + 24
+        }
+        return output
+    
+}
